02fe705c427b4593d16c6b1b524c8d050ebe9d3c
Bang210 <bangvelope@gmail.com>
2023-03-10 15:15:18 +0900

    first trial

---
+++ BAEKJOON_1032.py
@@ -0,0 +1,21 @@
+# 주어진 N개의 파일에 대해, 최소한의 '?'를 사용해 어떤 검색어로 검색했는지 구하는 문제
+
+n = int(input())
+result = list()
+
+for i in range(n) :
+    result.append(input())
+
+common = list()
+for l in range(len(result[0])) :
+    common.append(result[0][l])
+
+for j in range(n-1) :
+    for k in range(len(common)) :
+        if common[k] == '?' :
+            pass
+        elif common[k] != result[j+1][k] :
+            common[k] = '?'
+
+for m in range(len(common)) :
+    print(common[m], end='')
