3386ead68aa5dc12a7157e772a333645b537c6dc
Bang210 <bangvelope@gmail.com>
2023-03-16 12:57:44 +0900

    first trial

---
+++ BAEKJOON_2609.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+for i in range(min(a, b), 0, -1) :
+    if a % i == 0 and b % i == 0 :
+        maxi = i
+        break
+
+for j in range(max(a, b), a * b + 1) :
+    if j % a == 0 and j % b == 0 :
+        mini = j
+        break
+
+print(maxi)
+print(mini)
