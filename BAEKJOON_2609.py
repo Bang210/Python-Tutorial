f2150d08b97b12b130551e7d98122a33fd141316
Bang210 <bangvelope@gmail.com>
2023-03-16 13:16:18 +0900

    second trial

--- BAEKJOON_2609.py
+++ BAEKJOON_2609.py
@@ -1,5 +1,5 @@
 # 최대공약수와 최소공배수를 구하는 문제
-# 최소공배수를 구하는 과정에서 비효율이 발생한 것으로 보임.
+# first trial에서는 최소공배수를 구하는 과정에서 비효율이 발생한 것으로 보임.
 
 import sys
 input = sys.stdin.readline
@@ -16,4 +16,4 @@ least_common = int(a * b / greatest_common)
 print(greatest_common)
 print(least_common)
 
-# 시간초과
+# 정답
