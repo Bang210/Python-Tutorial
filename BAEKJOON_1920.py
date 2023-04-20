fc499332c192fe9db90ea21cc5aabd212e2f0366
Bang210 <bangvelope@gmail.com>
2023-04-20 17:41:49 +0900

    second trial

--- BAEKJOON_1920.py
+++ BAEKJOON_1920.py
@@ -8,21 +8,23 @@ universe = sorted(list(map(int, input().split())))
 test_num = int(input())
 test = list(map(int, input().split()))
 
-for i in range(test_num) :
-    arr = universe.copy()
+for i in range(test_num) :  
+    first_num = 0
+    last_num = len(universe) - 1
     while True :
-        middle_num = int((len(arr) - 1) * 0.5)
-        middle = arr[middle_num]
-        if test[i] == middle :
+        middle_num = (first_num + last_num) // 2
+        if test[i] == universe[middle_num] :
             check = 1
             break
-        elif (test[i] < arr[0]) or (test[i] > arr[len(arr) - 1]) :
+        elif (test[i] < universe[first_num]) or (test[i] > universe[last_num]) :
             check = 0
             break
-        elif test[i] > middle :
-            arr = arr[middle_num + 1 : ]
+        elif test[i] > universe[middle_num] :
+            first_num = middle_num + 1
         else :
-            arr = arr[ : middle_num]
+            last_num = middle_num
     print(check)
 
 # 시간초과. arr리스트를 슬라이싱하는 과정에서 시간이 초과된 것으로 보임.
+# 슬라이싱을 통해 리스트 자체를 수정하는 방법 대신, 기존 리스트의 탐색 범위를 새롭게 설정하는 방법으로 해결.
+# 정답.
