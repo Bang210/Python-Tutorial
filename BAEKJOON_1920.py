186ad76bd3837578d8b4c6fdf4b8c51ea8ff0ae0
Bang210 <bangvelope@gmail.com>
2023-04-20 17:19:09 +0900

    first trial

---
+++ BAEKJOON_1920.py
@@ -0,0 +1,29 @@
+# N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+
+import sys
+input = sys.stdin.readline
+    
+universe_num = int(input())
+universe = list(map(int, input().split()))
+test_num = int(input())
+test = list(map(int, input().split()))
+universe.sort()
+
+for i in range(test_num) :
+    arr = universe.copy()
+    while True :
+        middle_num = int((len(arr) - 1) * 0.5)
+        middle = arr[middle_num]
+        if test[i] == middle :
+            check = 1
+            break
+        elif (test[i] < arr[0]) or (test[i] > arr[len(arr) - 1]) :
+            check = 0
+            break
+        elif test[i] > middle :
+            arr = arr[middle_num + 1 : ]
+        else :
+            arr = arr[ : middle_num]
+    print(check)
+
+# 미해결
