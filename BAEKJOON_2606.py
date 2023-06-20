016ae720cfa761068c2757aca2ef9821ed1c2244
Bang210 <bangvelope@gmail.com>
2023-06-20 16:34:13 +0900

    second trial

--- BAEKJOON_2606.py
+++ BAEKJOON_2606.py
@@ -1,24 +1,31 @@
+# 컴퓨터의 수와 네트워크 연결 관계가 주어졌을 때, 1번 컴퓨터(감염됨)을 통해 바이러스에 감염되는 컴퓨터의 수를 구하는 문제
+
 import sys
+sys.setrecursionlimit(10 ** 8)
 input = sys.stdin.readline
 
-num_tests = int(input())
-ans = 0
+num_coms = int(input())
+num_connections = int(input())
 
-for i in range(num_tests) :
-    group = list()
-    word = input().rstrip()
-    check = 1
-    for j in range(len(word)) :
-        if j == 0 :
-            group.append(word[j])
-        else :
-            if word[j] == word[j - 1] :
-                pass
-            elif word[j] not in group :
-                group.append(word[j])
-            else :
-                check = 0
-                break
-    if check == 1 :
-        ans += 1
-print(ans)
+coms = [0 for _ in range(num_coms + 1)]
+visit = [0 for i in range(num_coms + 1)]
+graph = [[0] * (num_coms + 1) for j in range(num_coms + 1)]
+
+for k in range(num_connections) :
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+def virus(start) :
+    coms[start] = 1
+    visit[start] = 1
+    for l in range(1, num_coms + 1) :
+        if visit[l] == 0 and graph[start][l] == 1 :
+            virus(l)
+
+virus(1)
+print(coms.count(1) - 1)
+
+
+# dfs를 통해 해결
+# 정답
